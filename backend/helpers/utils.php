ee374875dc1dedf537a5cdaf33010f5991302597
Lucas Paniagua <you@example.com>
2025-06-08 23:01:36 -0300

    Log de errores.

--- backend/helpers/utils.php
+++ backend/helpers/utils.php
@@ -1,4 +1,11 @@
 <?php
+function logError($message) {
+    $logFile = __DIR__ . '/../logs/error.log';
+    $date = date('Y-m-d H:i:s');
+    $entry = "[$date] $message\n";
+    file_put_contents($logFile, $entry, FILE_APPEND | LOCK_EX);
+}
+
 function sendSuccess($message) {
     http_response_code(200);
     echo json_encode(["message" => $message]);
@@ -6,6 +13,12 @@ function sendSuccess($message) {
 }
 
 function sendError($code, $message) {
+    // Loguear el error antes de responder
+    $trace = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, 2);
+    $caller = isset($trace[1]) ? $trace[1] : null;
+    $location = $caller ? ($caller['file'] . ':' . $caller['line']) : '';
+    logError("[$code] $message $location");
+
     http_response_code($code);
     echo json_encode(["error" => $message]);
     exit;
